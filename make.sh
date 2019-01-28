91f8da9ae3be9bbf2d669a8272776c4be2fca809
Nick Nicholas <opoudjis@optusnet.com.au>
2019-01-28 01:36:22 +0000

    samples

---
+++ make.sh
@@ -0,0 +1,5 @@
+metanorma -t standoc -x xml nist-sp-800-90b.adoc
+metanorma -t standoc -x xml ogc-geojson.adoc
+metanorma -t standoc -x xml ogc-openapi.adoc
+metanorma -t standoc -x xml ogc-wfs.adoc
+metanorma -t standoc -x xml ss584_nn.adoc
